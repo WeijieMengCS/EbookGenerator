d71e72ca2d38cd5c51f9abb7d47d744065d7a2d8
Yuhong Zha <zha0@purdue.edu>
2017-02-27 12:57:09 -0500

    myselSetUp

---
+++ Ebook/mysql_dbc.js
@@ -0,0 +1,16 @@
+var mysql = require('mysql')
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password'
+})
+
+connection.connect()
+
+connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
+  if (err) throw err
+
+  console.log('You are now coonected to MySQL')
+})
+
+connection.end()
